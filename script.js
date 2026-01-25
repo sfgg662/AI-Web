e8997f233fdecdb93b2862e7e76a13da532a6d90
sfgg662 <2400013116@stu.pku.edu.cn>
2026-01-25 12:35:48 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// 简单的平滑滚动效果
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log("AI 写作助手已加载");
